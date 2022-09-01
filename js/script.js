cbceb76340abf6f9b7221a24ae98beb6aedd56e6
Natali <C:\Users\Oleksandr\AppData\Roaming\The Bat!>
2022-09-01 12:57:24 +0300

    Corrected quotes type

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const URL = 'https://jsonplaceholder.typicode.com/users';
 // .then/.catch
 fetch(URL)
     .then(response => response.json())
-    .then(users => console.log(users.filter(user => user.company.name === "Johns Group")))
+    .then(users => console.log(users.filter(user => user.company.name === 'Johns Group')))
     .catch(error => console.error(error));
 
 
@@ -14,7 +14,7 @@ fetch(URL)
 //     try {
 //         const response = await fetch(URL);
 //         const users = await response.json();
-//         console.log(users.filter(user => user.company.name === "Johns Group"));
+//         console.log(users.filter(user => user.company.name === 'Johns Group'));
 //     } catch (error){
 //         console.log(error);
 //         }
